22fc17d6862f97707f2ea303d83ebcaad76de6c4
Fidyeral <134742442+Fidyeral@users.noreply.github.com>
2023-08-28 16:34:23 -0600

    el mensaje fue modificado de nuevo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola ! mundo"); 
+Console.WriteLine("Hola ! mundo esto es una prueba de como editar desde git"); 
