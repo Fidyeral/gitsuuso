e80521420a61a90110d4e2ff4668ed26ab85c728
fidyeral <fidyeral2021@gmail.com>
2023-08-28 16:43:22 -0600

    se multiplico

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola ! mundo esto es una prueba de como editar desde git"); 
+Console.WriteLine("Hola ! mundo esto es una prueba de como editar desde git"); 
+Console.WriteLine("Hola ! mundo esto es una prueba de como editar desde git"); 
+Console.WriteLine("Hola ! mundo esto es una prueba de como editar desde git"); 
+Console.WriteLine("Hola ! mundo esto es una prueba de como editar desde git"); 
+Console.WriteLine("Hola ! mundo esto es una prueba de como editar desde git"); 
